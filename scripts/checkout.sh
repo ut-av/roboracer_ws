11aaa15a268a5f1412e22a046e92b3ecc99f84bf
Nathan <nathan@vertile.com>
2025-09-27 22:54:27 -0500

    builds with enml and graph nav

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -28,15 +28,21 @@ function clone_or_pull {
 }
 
 # common
-clone_or_pull ros2 https://github.com/ut-amrl/amrl_maps.git
-clone_or_pull master https://github.com/ut-amrl/amrl_msgs.git
+clone_or_pull ros2 https://github.com/ut-amrl/amrl_maps.git amrl_maps
+clone_or_pull master https://github.com/ut-amrl/amrl_msgs.git amrl_msgs
 
 # ut_automata
-clone_or_pull ros2 git@github.com:ut-amrl/ut_automata.git
+clone_or_pull ros2 git@github.com:ut-amrl/ut_automata.git ut_automata
 
-# Realsense
-REALSENSE_VERSION=v2.56.5-l4t36.4.4
-clone_or_pull $REALSENSE_VERSION https://github.com/nathantsoi/librealsense.git
+# graph nav
+clone_or_pull ros2_dev git@github.com:ut-amrl/graph_navigation.git graph_navigation
+
+# localization (enml)
+clone_or_pull ros2_dev git@github.com:ut-amrl/enml.git enml
+
+## Realsense
+#REALSENSE_VERSION=v2.56.5-l4t36.4.4
+#clone_or_pull $REALSENSE_VERSION https://github.com/nathantsoi/librealsense.git librealsense
 
 # MPU6050
-clone_or_pull main https://github.com/nathantsoi/ros2_mpu6050_driver.git
+clone_or_pull main https://github.com/nathantsoi/ros2_mpu6050_driver.git ros2_mpu6050_driver
--- scripts/ros2_deps.sh
+++ scripts/ros2_deps.sh
@@ -8,4 +8,5 @@ apt-get update && apt-get install -y \
   ros-$ROS_DISTRO-urg-node \
   ros-$ROS_DISTRO-diagnostic-updater \
   ros-$ROS_DISTRO-nav-msgs \
+  ros-$ROS_DISTRO-nav2-costmap-2d \
   ros-$ROS_DISTRO-foxglove-bridge
