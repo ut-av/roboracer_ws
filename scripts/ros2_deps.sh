b5f6cb49cc60680648538f1f667fa5c36afba71c
Nathan <nathan@vertile.com>
2025-10-28 23:12:03 -0500

    map creation

--- scripts/ros2_deps.sh
+++ scripts/ros2_deps.sh
@@ -9,4 +9,9 @@ apt-get update && apt-get install -y \
   ros-$ROS_DISTRO-diagnostic-updater \
   ros-$ROS_DISTRO-nav-msgs \
   ros-$ROS_DISTRO-nav2-costmap-2d \
+  ros-$ROS_DISTRO-tf2-geometry-msgs \
+  ros-$ROS_DISTRO-tf2-sensor-msgs \
+  ros-$ROS_DISTRO-slam-toolbox \
+  ros-$ROS_DISTRO-nav2-bringup \
+  ros-$ROS_DISTRO-navigation2 \
   ros-$ROS_DISTRO-foxglove-bridge
